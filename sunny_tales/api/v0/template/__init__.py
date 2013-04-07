2df1ffcac1fc2d0fd1eab9865797f0022fd230d2
Doris Ip <dorisip@gmail.com>
2013-04-06 23:03:56 -0400

    minor cleanup

--- sunny_tales/api/v0/template/__init__.py
+++ sunny_tales/api/v0/template/__init__.py
@@ -1,9 +1,9 @@
-from database.connection import create_db_client
+from database.db_manager import create_db_client
+
+
 def includeme(config):
     config.add_route('api.v0.template.new', '/api/v0/template/new')
     
-    # Include database
-    config.include('sunny_tales.database')
     # Create mongo client connection
     create_db_client()
     
--- sunny_tales/api/v0/template/create.py
+++ sunny_tales/api/v0/template/create.py
@@ -7,7 +7,7 @@ Created on Apr 2, 2013
 from pyramid.view import view_config
 import uuid
 from utils.element_loader import get_element_json
-from database.connection import insert
+from database.db_manager import insert
 
 @view_config(route_name='api.v0.template.new', request_method='GET', renderer='json')
 def new_template(request):
