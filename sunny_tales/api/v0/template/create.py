91e4934dab7fb14f5809c24099a0254249d6a3f5
Takashi Osako <takashi.osako@gmail.com>
2013-04-07 07:35:27 -0400

    fix reference

--- sunny_tales/api/v0/template/create.py
+++ sunny_tales/api/v0/template/create.py
@@ -5,7 +5,7 @@ Created on Apr 2, 2013
 '''
 
 from pyramid.view import view_config
-from database.db_manager import find_one
+from sunny_tales.database.db_manager import find_one
 
 @view_config(route_name='api.v0.template.new', request_method='GET', renderer='json')
 def new_template(request):
