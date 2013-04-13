c571236aa664ca8d698dcb19d3bb3e792eb3cb8f
Takashi Osako <takashi.osako@gmail.com>
2013-04-13 11:32:44 -0400

    fix bug

--- sunny_tales/static/js/models/report_component.js
+++ sunny_tales/static/js/models/report_component.js
@@ -1,10 +1,10 @@
 ReportComponent = Backbone.Model.extend({
-	initialize: function(tool) {
-		this.tool=tool;
-		this.set("html",tool.get("html"))
+	initialize : function(mode, tool) {
+		this.set("tool", tool);
+		this.set("html", tool.get("html"));
 	},
-	top: 0,
-	left: 0,
-	tool: null,
-	html: null
+	top : 0,
+	left : 0,
+	tool : null,
+	html : null
 });
--- sunny_tales/static/js/views/canvas.js
+++ sunny_tales/static/js/views/canvas.js
@@ -11,13 +11,15 @@ Canvas = Backbone.View.extend({
 				var component_id = ui.draggable.attr("id");
 				//get tool by id(type)
 				var tool = collection_tools.get(component_id);
-				var new_component = new ReportComponent(tool);
-				_components.add(new_component)
+				var new_component = new ReportComponent(null, tool);
+				_components.add(new_component);
 			}
 		});
 	},
 	addReportComponent : function(model_report_component) {
+		//rerender canvas when new tool is dragged from toolbox layout
 		new_component = $(model_report_component.get("html"));
-		new_component.appendTo($("#canvas"))
+		new_component.attr("id", model_report_component.cid);
+		new_component.appendTo($("#canvas"));
 	}
 });
