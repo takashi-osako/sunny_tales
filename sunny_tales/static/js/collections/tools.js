c3e8e18b8bc23e56d79fd062d9ad75f5d38c8a4b
Takashi Osako <takashi.osako@gmail.com>
2013-04-13 19:52:54 -0400

    refactored how to read JSON

--- sunny_tales/static/js/collections/tools.js
+++ sunny_tales/static/js/collections/tools.js
@@ -1,20 +1,14 @@
 ToolsCollection = Backbone.Collection.extend({
-    model : Tool,
-    url : '/api/v0/toolbox',
+	model : Tool,
+	url : '/api/v0/toolbox',
 
-    parse : function(response) {
-        if (response) {
-            var listSource = new Array();
-            _.each(response.elements, function(element, index, list) {
-                listSource.push(new Tool({
-                    "name": element.name,
-                    "html": element.html,
-                    "type": element.type,
-                    "resizable": element.reizable,
-                    "style": element.style
-                }));
-            });
-            return listSource;
-        }
-    }
+	parse : function(response) {
+		if (response) {
+			var listSource = new Array();
+			_.each(response.elements, function(element, index, list) {
+				listSource.push(new Tool(element));
+			});
+			return listSource;
+		}
+	}
 });
