8f1a201b6e7bc2071250c0ee12cc8f8853ac4608
Doris Ip <dorisip@gmail.com>
2013-04-14 01:27:51 -0400

    disable static resources cache, return better json responses from api

--- sunny_tales/__init__.py
+++ sunny_tales/__init__.py
@@ -5,7 +5,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'static', cache_max_age=0)
     config.add_static_view('templates', 'templates')
     config.add_route('home', '/')
     config.include('sunny_tales.api.v0.template', route_prefix='/api/v0')
