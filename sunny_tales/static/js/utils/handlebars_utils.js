3f41944a36e7b5248e90cc993fbe891ad106406f
Doris Ip <dorisip@gmail.com>
2013-04-20 11:53:45 -0400

    Use one style.template

--- sunny_tales/static/js/utils/handlebars_utils.js
+++ sunny_tales/static/js/utils/handlebars_utils.js
@@ -1,3 +1,10 @@
 Handlebars.registerHelper('function_isSelected', function(currentValue, defaultValue) {
     return currentValue === defaultValue ? "selected" : "";
 });
+
+Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+  if(v1 == v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
--- sunny_tales/static/js/views/StyleView.js
+++ sunny_tales/static/js/views/StyleView.js
@@ -1,18 +1,11 @@
 StyleView = Backbone.View.extend({
     el : $("#style"),
     renderStyle : function(target_id, array_style) {
-        var html_element = $(this.el);
-        html_element.empty();
-        var table=$("<table class='table'></table>");
-        table.data("target_id", target_id);
-        table.append("<tr><td>Property</td><td>Value</td></tr>");
-        _.each(array_style, function(style) {
-            var input_type = 'style.' + style.input_type + '.template';
-            var template = Handlebars.templates[input_type];
-            var html = template(style);
-            var row = $("<tr><td>" + style.name + "</td><td>" + html + "</td><tr>");
-            this.append(row);
-        }, table);
-        html_element.append(table);
+        var template = Handlebars.templates['style.template'];
+        data = {
+            "styles" : array_style,
+            "target_id" : target_id
+        }
+        $(this.el).html(template(data));
     }
 });
