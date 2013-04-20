16c0636276e325bf386916c65de6ba2882ddede5
Takashi Osako <takashi.osako@gmail.com>
2013-04-20 09:59:11 -0400

    add nicer view on style layout

--- sunny_tales/static/js/views/StyleView.js
+++ sunny_tales/static/js/views/StyleView.js
@@ -1,12 +1,16 @@
 StyleView = Backbone.View.extend({
     el : $("#style"),
     renderStyle : function(array_style) {
-        var html = "";
+        var html_element = $(this.el);
+        html_element.empty();
+        html_element.html("<table class='table'></table>");
+        html_element.append("<tr><td>Property</td><td>Value</td></tr>");
         _.each(array_style, function(style) {
             var input_type = 'style.' + style.input_type + '.template';
             var template = Handlebars.templates[input_type];
             var html = template(style);
-            $("#style").append(html);
-        });
+            var line = "<tr><td>"+style.name+"</td><td>"+html+"</td><tr>";
+            this.append(line);
+        }, html_element);
     }
 });
