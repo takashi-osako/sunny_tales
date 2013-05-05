c6ee048f58fe978b111a5dae085af327c9676a08
Doris Ip <dorisip@gmail.com>
2013-05-05 15:18:54 -0400

    remove mkdir

--- sunny_tales/utils/exporter.py
+++ sunny_tales/utils/exporter.py
@@ -9,19 +9,8 @@ import os
 import errno
 
 
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
-
-
 def export(data, tar_dir='/tmp'):
     file_name = os.path.join(tar_dir, 'template.json')
-    # mkdir_p(tar_dir)
     with open(file_name, 'w') as f:
         components = {"components": data['components']}
         content = json.dumps(components, indent=4, default=json_util.default)
